903f7f3892788f0976458bd2c11943dd64fa22b3
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-01-31 16:45:45 +0100

    initial commit

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/home': (context) => Home(),
+  },
+));
